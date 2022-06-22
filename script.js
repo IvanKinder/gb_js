eec608ddb8418c3cf6a89d9a705b35df83fcfc7e
Kinder <ivan.kinder@startinns.com>
2022-06-23 02:50:43 +0300

    hw6

--- script.js
+++ script.js
@@ -3,15 +3,51 @@ const BASE_URL =
 const GET_PRODUCTS_URL = `${BASE_URL}/catalogData.json`;
 const GET_BASKET_URL = `${BASE_URL}/getBasket.json`;
 
-function service(url, callback) {
-  fetch(url).then((responce) => {
-    responce.json().then((data) => {
-      callback(data);
-    });
-  });
-}
-
 window.onload = () => {
+  Vue.component("good", {
+    props: ["item"],
+    template: `
+    <div class="basket-item">
+      <h3>{{ item.product_name }}</h3>
+      <p>{{ item.price }}</p>
+    </div>
+    `,
+  });
+
+  Vue.component("search-field", {
+    props: ["searchValue"],
+    template: `
+    <input
+      class="search-field"
+      type="text"
+      placeholder="search product"
+      :value="searchValue"
+      @input="$emit('input', $event.target.value)"
+    />
+    `,
+  });
+
+  Vue.component("basket", {
+    props: ["items"],
+    template: `
+    <div class="basket">
+      <div class="basket-list">
+        <good v-for="item in items" :item="item"></good>
+      </div>
+    </div>
+    `,
+  });
+
+  Vue.component("err-msg", {
+    template: `
+    <transition name="err">
+      <div class="err-msg">
+        connection error
+      </div>
+    </transition>
+    `,
+  });
+
   const app = new Vue({
     el: "#root",
     data: {
@@ -22,13 +58,26 @@ window.onload = () => {
       ],
       searchValue: "",
       isVisibleCart: false,
+      serverError: false,
     },
     mounted() {
-      service(GET_PRODUCTS_URL, (products) => {
+      this.service(GET_PRODUCTS_URL, (products) => {
         this.products = products;
       });
     },
     methods: {
+      service(url, callback) {
+        fetch(url)
+          .then((responce) => {
+            responce.json().then((data) => {
+              callback(data);
+            });
+          })
+          .catch(() => {
+            this.serverError = true;
+            setTimeout(() => (this.serverError = false), 3000);
+          });
+      },
       getTitle() {
         return "eShop";
       },
